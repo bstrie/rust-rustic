8be567cdef7c2a0267053b6171910372ffb23422
Ben Striegel <ben.striegel@gmail.com>
2012-02-08 23:29:33 -0500

    Add dummy .rs file

---
+++ .rustic.rs
@@ -0,0 +1,18 @@
+use std;
+
+fn main() {
+    std::io::println("
+ Mr. Goodkat: The reason I'm in town, in case you're wondering, is because of a
+              Kansas City Shuffle.
+        Nick: What's a Kansas City Shuffle?
+ Mr. Goodkat: A Kansas City Shuffle is when everybody looks right, you go left.
+        Nick: Never heard of it.
+ Mr. Goodkat: It's not something people hear about. Falls on deaf ears mostly.
+              This particular one has been over twenty years in the making.
+        Nick: Twenty years, huh?
+ Mr. Goodkat: No small matter. Requires a lot of planning. Involves a lot of
+              people. People connected only by the slightest of events. Like
+              whispers in the night, in that place that never forgets, even
+              when those people do.
+ Mr. Goodkat: It starts with a horse.");
+}
